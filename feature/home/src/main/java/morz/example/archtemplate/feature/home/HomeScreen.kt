ed00e3de2a41e8ec6bad12cc804a1053abdfaba9
morz <morz@gmail.com>
2025-09-06 10:34:10 +0330

    feature-grid: Implementing ToDoGridItem, ToDoGrid

--- feature/home/src/main/java/morz/example/archtemplate/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/morz/example/archtemplate/feature/home/HomeScreen.kt
@@ -1,11 +1,15 @@
 package morz.example.archtemplate.feature.home
 
+import android.annotation.SuppressLint
+import android.service.quicksettings.Tile
 import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,7 +17,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -33,11 +43,14 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -47,6 +60,7 @@ import morz.example.archtemplate.core.designsystem.component.AppLoadingView
 import morz.example.archtemplate.core.designsystem.theme.AppTheme
 import morz.example.archtemplate.core.model.ToDo
 import morz.example.archtemplate.core.ui.DevicePreviews
+import kotlin.random.Random
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -121,7 +135,7 @@ internal fun HomeScreen(
             Box(
                 modifier = Modifier.fillMaxSize()
             ) {
-                ToDoList(homeUiState.homeData.data, onItemClick, modifier)
+                ToDoGrid(homeUiState.homeData.data, onItemClick, modifier)
             }
         }
 
@@ -148,6 +162,42 @@ fun ToDoList(
                 todo = todos[index],
                 onItemClick = onItemClick,
                 modifier = modifier
+            )
+        }
+    }
+}
+
+@SuppressLint("ConfigurationScreenWidthHeight")
+@Composable
+fun ToDoGrid(
+    todos: List<ToDo>,
+    onItemClick: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val columnWidth = if (screenWidth > 600.dp) {
+        120.dp
+    } else { // Mobile
+        150.dp
+    }
+
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Adaptive(columnWidth),
+        contentPadding = PaddingValues(4.dp),
+        verticalItemSpacing = 4.dp,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    )
+    {
+        items(
+            todos.size,
+            { index -> todos[index].id ?: 0 }
+        ) { index ->
+            ToDoGridItem(
+                todo = todos[index],
+                onItemClick = onItemClick,
+                modifier = modifier,
             )
         }
     }
@@ -196,6 +246,59 @@ fun ToDoItem(
                 contentDescription = "todo status image",
                 modifier = Modifier.size(40.dp)
             )
+        }
+    }
+}
+
+@Composable
+fun ToDoGridItem(
+    modifier: Modifier = Modifier,
+    todo: ToDo,
+    onItemClick: (Int) -> Unit,
+){
+    Card(
+        modifier = modifier
+            .wrapContentSize()
+            .padding(16.dp)
+            .clickable { onItemClick(todo.id!!) },
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.Start
+        ){
+
+            Text(
+                text = "To Do #${todo.id}",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = todo.todo ?: "",
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Image(
+                painter =
+                    painterResource(
+                        if (todo.completed ?: false)
+                            R.drawable.ic_completed
+                        else
+                            R.drawable.ic_not_completed,
+                    ),
+                contentDescription = "todo status image",
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(top = 4.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
         }
     }
 }
@@ -277,3 +380,26 @@ private fun HomeScreenPreview() {
         )
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@DevicePreviews
+@Composable
+private fun ToDoGridItemPreview() {
+    val todo = ToDo(1, "adasdasd", true, 123)
+    ToDoGridItem(
+        todo = todo,
+        onItemClick = {},
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@DevicePreviews
+@Composable
+private fun ToDoListItemPreview() {
+    val todo = ToDo(1, "adasdasd", true, 123)
+    ToDoItem(
+        todo = todo,
+        onItemClick = {},
+    )
+}
+
